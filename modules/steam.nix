57f5d4aea9c95354667449fa55559a93f96c1e48
spaceey1 <bruno@gadula.ca>
2025-12-19 13:48:05 +0100

    gayming

--- modules/steam.nix
+++ modules/steam.nix
@@ -4,4 +4,8 @@
 	hardware.steam-hardware.enable = true;
 	programs.steam.enable = true;
 	programs.gamescope.enable = true;
+	environment.systemPackages = with pkgs; [
+		r2modman
+		mangohud
+	];
 }
