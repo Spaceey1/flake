76e66b43aa559954b3bbf736cdd1be959df0f481
spaceey1 <bruno@gadula.ca>
2026-02-12 01:21:17 +0100

    lighthouse vr package

--- modules/vr.nix
+++ modules/vr.nix
@@ -3,10 +3,14 @@
   lib,
   inputs,
   pkgs,
+  stdenv,
   ...
 }:
 
 {
+  imports = [
+    ./bluetooth.nix
+  ];
   nixpkgs.overlays = [
     inputs.nixpkgs-xr.overlays.default
     (final: prev: {
@@ -18,10 +22,44 @@
         ];
       });
     })
+
+    (final: prev: {
+      vr-lighthouse = final.callPackage (
+        {
+          stdenv,
+          fetchzip,
+          autoPatchelfHook,
+          udev,
+          dbus,
+        }:
+        stdenv.mkDerivation rec {
+          pname = "vr-lighthouse";
+          version = "1.4.0";
+          src = fetchzip {
+            url = "https://github.com/ShayBox/Lighthouse/releases/download/${version}/Linux-x86_64.zip";
+            sha256 = "sha256-ZseNwoU++R1LmGIfyCGwLADG4TC4mnpQju0JafrMAWY=";
+            stripRoot = false;
+          };
+          nativeBuildInputs = [ autoPatchelfHook ];
+          buildInputs = [
+            udev
+            dbus
+            stdenv.cc.cc.lib
+          ];
+          installPhase = ''
+            runHook preInstall
+            install -Dm755 lighthouse $out/bin/lighthouse
+            runHook postInstall
+          '';
+        }
+      ) { };
+    })
   ];
   environment.systemPackages = with pkgs; [
     xrizer
-wayvr
+    wayvr
+    xrbinder
+    vr-lighthouse
   ];
   my.python.packages = lib.mkAfter (
     with pkgs.python3Packages;
